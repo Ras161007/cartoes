5cf213f8f09deb93e44ebfb314d528f1a38e2184
Ras161007 <168222507+Ras161007@users.noreply.github.com>
2024-11-28 09:36:25 -0300

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    let respostaEstaVisivel = false
+    container.appendChild(cartao)
+}
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
